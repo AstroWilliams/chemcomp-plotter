b3ebb50fec3ed651da9985256fdcd6f8d8a412a7
jpw447 <joepw1@hotmail.com>
2024-09-24 13:39:52 +0100

    adding setup files

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1' 
+VERSION = '0.3.7'
 DESCRIPTION = 'Load disc data from chemcomp simulations.'
 LONG_DESCRIPTION = 'Python package to load disc data from simulations from the semi-analytical viscous disc code "chemcomp" by Schneider & Bitsch 2021.'
 
@@ -13,8 +13,9 @@ setup(
         author_email="joepw1@hotmail.com",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
+        download_url='https://github.com/AstroWilliams/chemcomp-plotter/archive/refs/tags/v_0.1.tar.gz',
         packages=find_packages(),
-        install_requires=['tables'], # add any additional packages that 
+        install_requires=['tables', 'numpy', 'matplotlib', 'yaml', 'ast', 'astropy'], # add any additional packages that 
         # needs to be installed along with your package. Eg: 'caer'
         
         keywords=['python', 'chemcomp'],
